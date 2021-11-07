de7c7578ba5eb75b289aebfcd9bc202fb5ee2880
Dark Side <85755442+Kasperskybest@users.noreply.github.com>
2021-11-07 17:12:27 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,26 +1,37 @@
-{
-  "name": "Swetie's AI",
-  "version": "1.0.0",
-  "description": "A simple Node app built on Express, instantly up and running.",
-  "main": "server.js",
-  "scripts": {
-    "start": "node server.js"
-  },
-  "dependencies": {
-    "chatbot-zero": "^1.1.0",
-    "smartestchatbot": "^2.0.1",
-    "discord.js": "^13.2.0"
-  },
-  "engines": {
-    "node": "16.x"
-  },
-  "repository": {
-    "url": "https://glitch.com/edit/#!/hello-express"
-  },
-  "license": "MIT",
-  "keywords": [
-    "node",
-    "glitch",
-    "express"
-  ]
-}
+const { Client } = require("discord.js");
+const client = new Client({
+  intents: ["GUILDS", "GUILD_MESSAGES", "DIRECT_MESSAGES"],
+  partials: ["CHANNEL"]
+});
+const smartestchatbot = require("smartestchatbot");
+const x = new smartestchatbot.Client();
+client.on("ready", () => {
+  console.log("Ready for chatting!| Bot by Dark side");
+});
+client.on("messageCreate", async message => {
+  // when client detects a message
+  if (message.author.bot) return; // if the author of the message is a bot ignore the case
+  message.content = message.content
+    .replace(/@(everyone)/gi, "everyone")
+    .replace(/@(here)/gi, "here");
+  if (message.content.includes(`@`)) {
+    return message.reply({
+      content: `**:x: Please dont mention anyone while talking to me I feel attacked 😭**`
+    });
+  }
+  if (!message.content)
+    return message.reply({
+      content: "I can only reply to text messages"
+    });
+  x.chat({
+    message: message.content,
+    name: client.user.username,
+    owner: "NOBITA#2433",
+    user: message.author.id,
+    language: "en"
+  }).then(reply => {
+    message.channel.sendTyping();
+    message.reply(`${reply}`);
+  });
+});
+client.login(process.env.TOKEN); //login using the token
